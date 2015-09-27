36bfadb2fb09883b4026d2e92ce86888bae230f0
Klajdi Bejko <kbejko@gmail.com>
2015-09-27 17:07:03 -0400

    Add landing elements

--- parallax/_site/assets/js/functions.js
+++ parallax/_site/assets/js/functions.js
@@ -11,7 +11,18 @@ $(window).scroll(function(){
   });
 
   $('.fore-bird').css({
-    'transform' : 'translate(0px, -'+ wScroll / 80 +'%)'
+    'transform' : 'translate(0px, -'+ wScroll / 100 +'%)'
   });
+
+  if(wScroll > $('.clothes-pics').offset().top - ($(window).height() / 1.2)) {
+
+    $('.clothes-pics figure').each(function(i){
+
+      setTimeout(function(){
+        $('.clothes-pics figure').eq(i).addClass('is-showing');
+    }, 150 * (i+1));
+    });
+
+  }
 
 });
--- parallax/gulpfile.js
+++ parallax/gulpfile.js
@@ -43,7 +43,7 @@ gulp.task('browser-sync', ['sass', 'jekyll-build'], function() {
 gulp.task('sass', function () {
     return gulp.src('assets/css/style.scss')
         .pipe(sass({
-            includePaths: ['css'],
+            includePaths: ['css', 'scss'],
             onError: browserSync.notify
         }))
         .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
